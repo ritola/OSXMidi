600ef127d95e2e27ad1e6c6e54557ba04fc9a371
Heikki Ritola <heikki.ritola@oneten.cx>
2011-09-04 19:39:56 +0300

    remove ridiculous default comments

--- OSXMidi/OSXMidi.h
+++ OSXMidi/OSXMidi.h
@@ -1,11 +1,3 @@
-//
-//  OSXMidi.h
-//  OSXMidi
-//
-//  Created by Heikki Ritola on 04.09.2011.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 @interface OSXMidi : NSObject
--- OSXMidi/OSXMidi.m
+++ OSXMidi/OSXMidi.m
@@ -1,11 +1,3 @@
-//
-//  OSXMidi.m
-//  OSXMidi
-//
-//  Created by Heikki Ritola on 04.09.2011.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
-
 #import "OSXMidi.h"
 
 @implementation OSXMidi
