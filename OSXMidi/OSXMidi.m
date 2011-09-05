ba8f88fb88ce23adb1b734bffb6abfebd5756900
Heikki Ritola <heikki.ritola@oneten.cx>
2011-09-05 19:54:34 +0300

    remove unneeded stuff from template

--- OSXMidi/OSXMidi.m
+++ OSXMidi/OSXMidi.m
@@ -1,15 +1,1 @@
 #import "OSXMidi.h"
-
-@implementation OSXMidi
-
-- (id)init
-{
-    self = [super init];
-    if (self) {
-        // Initialization code here.
-    }
-    
-    return self;
-}
-
-@end
