914c6e5192cbe0af98cb9d88576fda6c5a6a3a20
Heikki Ritola <heikki.ritola@oneten.cx>
2011-10-05 22:50:39 +0300

    add entity and device

--- java/src/main/scala/cx/oneten/osxmidi/jni/MidiEndpoint.scala
+++ java/src/main/scala/cx/oneten/osxmidi/jni/MidiEndpoint.scala
@@ -1,7 +1,19 @@
 package cx.oneten.osxmidi.jni
 
-class MidiEndpoint {
+class MidiObject {
   var ref: Long = 0
-  var name: String = ""
-  override def toString = "[MidiEndpoint: ref=" + ref + " name=" + name + "]"
+  var properties: Map[String, String] = new HashMap
+  override def toString = "[MidiObject: ref=" + ref + " properties=" + properties + "]"
+}
+
+class MidiEndpoint extends MidiObject {
+  entity: MidiEntity
+}
+
+class MidiEntity extends MidiObject {
+  device: MidiDevice
+}
+
+class MidiDevice extends MidiObject {
+
 }
