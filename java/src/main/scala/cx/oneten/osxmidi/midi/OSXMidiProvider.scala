96444581aeb04044feaede84a8958c7b250161b8
Heikki Ritola <heikki.ritola@oneten.cx>
2011-10-17 21:29:57 +0300

    provide midi device info

--- java/src/main/scala/cx/oneten/osxmidi/midi/OSXMidiProvider.scala
+++ java/src/main/scala/cx/oneten/osxmidi/midi/OSXMidiProvider.scala
@@ -1,10 +1,23 @@
 package cx.oneten.osxmidi.midi
 
+import scala.collection.JavaConversions._
+
 import javax.sound.midi.spi.MidiDeviceProvider
 import javax.sound.midi.MidiDevice
 import javax.sound.midi.MidiDevice.Info
 
+import cx.oneten.osxmidi.OSXMidi
+import cx.oneten.osxmidi.jni.MidiEndpoint
+
 class OSXMidiProvider extends MidiDeviceProvider {
   override def getDevice(info: Info): MidiDevice = { null }
-  override def getDeviceInfo(): Array[Info] = { null }
+  override def getDeviceInfo(): Array[Info] = {
+    val o = new OSXMidi
+    o.getEndpoints.map(new OSXMidiInfo(_)).toArray
+  }
+}
+
+class OSXMidiInfo(e: MidiEndpoint)
+  extends Info(e.properties.get("name"), e.entity.get.device.get.properties.get("name"), e.properties.get("name"), "1.0") {
+
 }
